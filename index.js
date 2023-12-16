ba1a2f2b3b00be6a61bb050beb491111ae405d59
Sarahbryant411 <sarahbryant411@gmail.com>
2023-12-16 16:48:37 -0500

    added function to intialize the app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -62,10 +63,16 @@ const questions = [{
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data)=>{console.log(data);
+    })
+}
 
 // Function call to initialize app
 init();
